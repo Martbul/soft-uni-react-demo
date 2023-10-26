0727acd6ba0efdd55c791c46b4815ff429a478b2
martbul <martbul01@gmail.com>
2023-10-26 16:53:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Header from "./components/Header";
+import OurServices from "./components/OurServices";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import WhySection from "./components/WhySection";
+import OurTeam from "./components/OurTeam";
+import Info from "./components/Info";
+
+function App() {
+  return (
+
+    <div>
+      <Header />
+      
+      <OurServices />
+
+      <About />
+
+      <WhySection />
+
+      <OurTeam />
+      
+      <Info />
+
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
